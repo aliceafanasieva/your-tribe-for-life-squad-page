dd129c60f521b135c83dcb11271ff4d96e7cb045
Abeeryu <144008500+Abeeryu@users.noreply.github.com>
2025-09-08 15:15:17 +0200

    add member details aan detailspagina
    Refs: #25

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,15 @@
   let { data } = $props();
 
   const members = data.members;
+
+  
 </script>
 
 <h1>Welcome to Sqaudpage</h1>
 
 {#each members as member}
   <a href={member.id}>{member.name}</a>
+  
 {/each}
 
 <style>
--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -2,7 +2,25 @@
   let { data } = $props();
 
   const member = data.member;
+
 </script>
 
 <a href="/">Terug naar overzicht</a>
-<h1>{member.name}</h1>
+
+<section>
+  <div></div>
+
+  <img
+      src="https://fdnd.directus.app/assets/{member.mugshot}?fit=cover&width=1600"
+      alt={member.name}
+    />
+
+  <h1>{member.name}</h1>
+  <h2>{member.bio}</h2>
+  <h3>Link naar mijn visitkaartje</h3>
+
+</section>
+
+
+
+
