a19d5bfea6db06b7649fa55073a1a04c99cca54f
Alisa <alice.afanasieva@gmail.com>
2025-09-12 12:38:00 +0200

    refactor(styles): global & page CSS toegevoegd;
    - src/lib/styles/global.css gemaakt
    
    refs #28, #19

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,10 @@
 <script>
+  import "$lib/styles/global.css";
   import favicon from "$lib/assets/favicon.svg";
-
-  let { children } = $props();
 </script>
 
 <svelte:head>
   <link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<slot />
