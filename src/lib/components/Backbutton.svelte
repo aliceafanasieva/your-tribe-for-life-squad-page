3c81642dcfb0986a9656a909f104ba0ef0cae81e
Abeeryu <144008500+Abeeryu@users.noreply.github.com>
2025-09-15 16:04:31 +0200

    feat: components toevoegen
    refs: #60

---
+++ src/lib/components/Backbutton.svelte
@@ -0,0 +1,23 @@
+<script>
+    let { data } = $props();
+    const member = data.member;
+</script>
+
+<a href="/" class="back-button">← Terug naar overzicht</a>
+
+<style>
+
+.back-button {
+  display: inline-block;
+  margin: 1rem 1rem;
+  text-decoration: none;
+  color: #000000;
+  font-weight: 600;
+  transition: color 0.2s;
+}
+
+.back-button:hover {
+  color: #a14ed1;
+}
+
+</style>
